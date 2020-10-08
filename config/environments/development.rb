a8219741a58da34d2d419aff7717d57e88d12876
Marko Anton Potočnik <marko@globalwebindex.com>
2020-10-08 17:30:56 +0200

    Fix typo in hostname. It doesn't redirect without it.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,5 +60,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.x.host = 'htts://localhost:3000'
+  config.x.host = 'http://localhost:3000'
 end
