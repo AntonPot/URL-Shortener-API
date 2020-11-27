318ad3284771b2b5415183c37a9c1f047e033d95
Marko Anton Potočnik <markoapotocnik@hotmail.com>
2020-11-27 08:29:07 +0100

    comments added

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,4 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  # NOTE: Exposing headers to get filename
+  # https://glaucocustodio.github.io/2016/01/20/dont-forget-to-expose-headers-when-using-rack-cors/
   allow do
     origins 'http://localhost:3000'
     resource '*',
